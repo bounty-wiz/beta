3bb18da6ced5431c7504708c30871324d6ce28e3
Andrey Obruchkov <andrey.obruchkov@replix.io>
2021-12-21 18:37:32 +0200

    added hello world program

---
+++ C/hello_world/hello_world.c
@@ -0,0 +1,20 @@
+/**
+ * @file hello_world.c
+ * @brief prints hello world
+ *
+ * 
+ *
+ * version log
+ * ------------
+ * version 0.1.0 by bounty_wiz, 21.12.2021
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world\n");
+}
+
+
